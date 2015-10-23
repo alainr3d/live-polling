63846564ab09b23ef19fcb22ab30f629ef414e70
Richard Dilig <rdilig@gluix.com>
2015-10-23 05:33:06 +0000

    fix app server

--- app-server.js
+++ app-server.js
@@ -12,9 +12,9 @@ var app = (0, _express2['default'])();
 app.use(_express2['default']['static']('./public'));
 app.use(_express2['default']['static']('./node_modules/bootstrap/dist'));
 
-//app.listen(process.env.PORT, process.env.IP);
-//console.log("Polling server is running");
+app.listen(process.env.PORT, process.env.IP);
+console.log("Polling server is running");
 
-app.listen(process.env.PORT, function () {
-  console.log('listening on', app.address().port);
-});
+//app.listen(process.env.PORT, function () {
+//  console.log('listening on', app.address().port);
+//});
